87ca2f53b9f6cb8e8e99d6f8be250d64552d4f81
Vik63 <sfoto116@yandex.ru>
2025-11-26 18:34:21 +0400

    add ard_3

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,6 @@ import * as cookieParser from 'cookie-parser';
 import * as cors from 'cors';
 import * as passport from 'passport';
 
-const httpsOptions = {
-  key: readFileSync('../security/photostudio.ru+3-key.pem'),
-  cert: readFileSync('../security/photostudio.ru+3.pem'),
-};
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
