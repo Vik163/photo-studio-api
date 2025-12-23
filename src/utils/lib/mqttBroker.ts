fd83b82dd49ab3decd8d69e4825c90efe9086030
Vik63 <sfoto116@yandex.ru>
2025-12-23 14:09:50 +0400

    change push

--- src/utils/lib/mqttBroker.ts
+++ src/utils/lib/mqttBroker.ts
@@ -7,6 +7,8 @@ const topicTimer = 'home/timer';
 
 const connectUrl = `mqtt://${process.env.ARD_MQTT_HOST}:${process.env.ARD_MQTT_PORT}`;
 
+let keyPush = false;
+
 const client = mqtt.connect(connectUrl, {
   // clientId, // подписывается только один раз
   connectTimeout: 4000,
@@ -19,14 +21,27 @@ client.on('connect', () => {
   client.subscribe([topicStatus, topicTimer], () => {});
 });
 
-client.on('message', (topic, payload) => {
+client.on('message', async (topic, payload) => {
   const status = payload.toString() as Status;
   const text = `Статус дома **${status}**`;
 
   if (topic === topicStatus) {
-    fetch(
-      `https://api.telegram.org/bot${process.env.ARD_TELEG_TOKEN}/sendMessage?chat_id=${process.env.ARD_TELEG_ID}&text=${text}`,
-    );
+    if (status === 'online' && keyPush) {
+      keyPush = false;
+      await fetch(
+        `https://api.telegram.org/bot${process.env.ARD_TELEG_TOKEN}/sendMessage?chat_id=${process.env.ARD_TELEG_ID}&text=${text}`,
+      );
+    }
+    setTimeout(async () => {
+      if (status === 'offline') {
+        keyPush = true;
+        await fetch(
+          `https://api.telegram.org/bot${process.env.ARD_TELEG_TOKEN}/sendMessage?chat_id=${process.env.ARD_TELEG_ID}&text=${text}`,
+        );
+      } else {
+        keyPush = false;
+      }
+    }, 20000);
   }
 });
 
